729381d9cf0fbccdd2be3c3a6efa3e5b0aecebc1
gouththam <gouththam302@gmail.com>
2018-09-26 12:14:52 +0530

    this is file

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '511346602660154', // your App ID
         'clientSecret'  : 'abfa559f286ad54c6fff082686581286', // your App Secret
-        'callbackURL'   : 'http://assigment3bygouththam.herokuapp.com/auth/facebook/callback',
+        'callbackURL'   : 'https://assigment3bygouththam.herokuapp.com/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
       },
